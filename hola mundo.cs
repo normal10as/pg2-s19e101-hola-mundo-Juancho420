03793ca8d6c48f9f34085cc0ec0d1a6944810dfb
Juancho420 <63302026+Juancho420@users.noreply.github.com>
2020-04-07 11:51:10 -0300

    Add files via upload

---
+++ hola mundo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hola_Mundo_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
